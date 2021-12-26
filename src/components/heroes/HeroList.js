b8bb0fc1b32df0b9f556315b122851c19489775e
Edgard Vilo <evilo@hostyweb.cl>
2021-12-26 13:00:40 -0300

    Clase 176: Tarjetas con la informacón del Heroe

--- src/components/heroes/HeroList.js
+++ src/components/heroes/HeroList.js
@@ -1,20 +1,20 @@
-import { getHeroesByPublisher } from "../selectors/getHeroesByPublisher"
+import { getHeroesByPublisher } from '../selectors/getHeroesByPublisher';
+import { HeroCard } from "./HeroCard";
 
 export const HeroList = ({ publisher }) => {
 
     const heroes = getHeroesByPublisher(publisher);
 
     return (
-        <>
-            <ul>
-                {
-                    heroes.map( hero => (
-                        <li key={hero.id}>
-                            { hero.superhero }
-                        </li>
-                    ))
-                }
-            </ul>
-        </>
+        <div className="row row-cols-1 row-cols-md-3 g-3">
+            {
+                heroes.map( hero => (
+                    <HeroCard 
+                        key={hero.id} 
+                        { ...hero } 
+                    />
+                ))
+            }
+        </div>
     )
 }
