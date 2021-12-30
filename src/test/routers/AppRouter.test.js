7287ea958beb4bc58e32a7e24878e7946a4adaa8
Edgard Vilo <evilo@hostyweb.cl>
2021-12-29 21:03:16 -0300

    Clase 203: Pruebas AppRouter - Usuario autenticado

--- src/test/routers/AppRouter.test.js
+++ src/test/routers/AppRouter.test.js
@@ -4,13 +4,14 @@ import { AppRouter } from '../../routers/AppRouter';
 
 describe('Pruebas en <AppRouter />', () => {
 
-    const contextValue = {
-        user: {
-            logged: false,
-        }
-    };
     
     test('debe de mostrar el login si no está autenticado', () => {
+
+        const contextValue = {
+            user: {
+                logged: false,
+            }
+        };
         
         const wrapper = mount( 
             <AuthContext.Provider value={contextValue}>
@@ -20,6 +21,27 @@ describe('Pruebas en <AppRouter />', () => {
 
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('h1').text().trim()).toBe('Login Screen');
+        
+    });
+
+
+    test('debe de mostrar el componente de Marvel si está autenticado', () => {
+
+        const contextValue = {
+            user: {
+                logged: true,
+                name: 'Edgard'
+            }
+        };
+        
+        const wrapper = mount( 
+            <AuthContext.Provider value={contextValue}>
+                <AppRouter />
+            </AuthContext.Provider>
+        );
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('.navbar').exists()).toBeTruthy();
         
     });
 
