b07329c253b66a16a54136ad3dc177b67e088943
Edgard Vilo <evilo@hostyweb.cl>
2021-12-31 09:09:35 -0300

    Clase 205: Probar rutas dentro del DashboardRoutes

--- src/test/routers/DashBoardRoutes.test.js
+++ src/test/routers/DashBoardRoutes.test.js
@@ -12,17 +12,33 @@ describe('Pruebas en <DashboardRoutes />', () => {
         }
     }
     
-    test('debe de mostrarse correctamente', () => {
+    test('debe de mostrarse correctamente de Marvel /', () => {
         
         const wrapper = mount(
             <AuthContext.Provider value={contextValue}>
-                <MemoryRouter>
+                <MemoryRouter initialEntries={ ['/'] }>
                     <DashboardRoutes />
                 </MemoryRouter>
             </AuthContext.Provider> 
         );
         expect(wrapper).toMatchSnapshot();
         expect(wrapper.find('.text-info').text().trim()).toBe('Edgard');
+        expect(wrapper.find('h1').text().trim()).toBe('Marvel Screen');
+
+    });
+
+
+    test('debe de mostrarse correctamente de DC /', () => {
+        
+        const wrapper = mount(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={ ['/dc'] }>
+                    <DashboardRoutes />
+                </MemoryRouter>
+            </AuthContext.Provider> 
+        );
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('h1').text().trim()).toBe('DC Screen');
 
     });
     
