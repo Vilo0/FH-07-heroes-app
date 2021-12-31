eaeb912898712ff23dda234fb41aee047b7b6338
Edgard Vilo <evilo@hostyweb.cl>
2021-12-31 09:18:39 -0300

    Clase 206: Pruebas en el SearchScreen

--- src/selectors/getHeroesByName.js
+++ src/selectors/getHeroesByName.js
@@ -1,8 +1,7 @@
 import { heroes } from '../data/heroes';
 
 export const getHeroesByName = (name = '') => {
-    console.log('gerByHeroesByName called');
-
+    //console.log('gerByHeroesByName called');
     name = name.toLowerCase();
     if(name === '') return [];
     return heroes.filter(hero => hero.superhero.toLowerCase().includes(name));
